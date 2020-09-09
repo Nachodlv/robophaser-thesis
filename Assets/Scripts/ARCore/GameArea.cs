4d77e32fef12e2b75bb26dfca43a0ecfd388cd1c
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-09-09 19:27:09 -0300

    Implement game area resize mechanics

---
+++ Assets/Scripts/ARCore/GameArea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ARCore
+{
+    public class GameArea : MonoBehaviour
+    {
+        [SerializeField] private Slider xSlider;
+        [SerializeField] private Slider zSlider;
+
+        private void Awake()
+        {
+            var localScale = transform.localScale;
+            xSlider.value = localScale.x;
+            zSlider.value = localScale.z;
+
+            xSlider.onValueChanged.AddListener(ScaleInX);
+            zSlider.onValueChanged.AddListener(ScaleInZ);
+        }
+
+        private void ScaleInX(float scale)
+        {
+            var transform1 = transform;
+            var transformLocalScale = transform1.localScale;
+            transformLocalScale.x = scale;
+            transform1.localScale = transformLocalScale;
+        }
+
+        private void ScaleInZ(float scale)
+        {
+            var transform1 = transform;
+            var transformLocalScale = transform1.localScale;
+            transformLocalScale.z = scale;
+            transform1.localScale = transformLocalScale;
+        }
+    }
+}
