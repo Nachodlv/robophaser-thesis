1395eacf0c9be1c1afd8de7f51cf0496e4c6521c
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-02-15 11:09:53 -0300

    Fix health displayer when scene is restarted

--- Assets/Scripts/UI/Combat/Stats/HealthDisplayer.cs
+++ Assets/Scripts/UI/Combat/Stats/HealthDisplayer.cs
@@ -11,12 +11,21 @@ namespace UI.Combat
     {
         [SerializeField] private StatBarSlider slider;
 
+        private PhotonPlayer _player;
+
         public void DisplayHealth(PhotonPlayer player)
         {
+            if (slider.gameObject == null) return;
             if (!slider.gameObject.activeSelf) slider.gameObject.SetActive(true);
             slider.MaxValue = player.MaxHealth;
             slider.Value = player.MaxHealth;
-            player.OnHealthUpdate += UpdateHealth;
+            _player = player;
+            _player.OnHealthUpdate += UpdateHealth;
+        }
+
+        private void OnDestroy()
+        {
+            if(_player != null) _player.OnHealthUpdate -= UpdateHealth;
         }
 
         private void UpdateHealth(int currentHealth)
